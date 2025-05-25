1a5661d576390e01d6ab1b01516a54cef4beb638
Dean Cabral <26599041+DeanCabral@users.noreply.github.com>
2025-05-25 23:11:44 +0100

    Added hero banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import ic_play from '../../assets/play_icon.png'
+import ic_info from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      <div className="hero">
+        <img src={hero_banner} alt="" className='banner-img'/>
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className='caption-img'/>
+          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={ic_play} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={ic_info} alt="" />More Info</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
